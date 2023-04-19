31539bd0d82bc5205e93454ade4c2dfec8ae5012
SoftTouchSys <99830414+Carcess@users.noreply.github.com>
2023-04-19 20:33:59 +0500

    render html file

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ const mongoose = require('mongoose');
 const app = express();
 
 const env = require('dotenv');
-env.config();
-mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true});
 
-mongoose.connection.on('connected', function () {  
+env.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
+
+mongoose.connection.on('connected', function () {
     console.log('Mongodb connected');
-  }); 
-  
+});
+
+
+
+
+// Render HTML file
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+
+});
 
 app.listen(3000, () => console.log('Server running on port 3000'));
 
